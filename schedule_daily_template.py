883ec1824bcb90648cc2b103a120c946f386d61f
Ivan Yugov <iyugov@gmail.com>
2022-08-06 22:59:39 +0300

    Rename Daily Schedule to Schedule Dailt Template

--- schedule_daily_template.py
+++ schedule_daily_template.py
@@ -1,11 +1,11 @@
-'''Schedule within a day.'''
+'''Schedule template within a day.'''
 
 from lesson import Lesson
 from time_of_day import TimeOfDay
 from json import dump
 
 
-class DailySchedule():
+class ScheduleDailyTemplate():
     '''Schedule within a day.'''
     lessons: list[Lesson] = []
     events: list[TimeOfDay] = []
