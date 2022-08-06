3a36e1ea657adc0fab6ecb69496cf5458fd2c5cb
Ivan Yugov <iyugov@gmail.com>
2022-08-06 20:52:50 +0300

    Update class properties.

--- daily_schedule.py
+++ daily_schedule.py
@@ -1,10 +1,11 @@
 '''Schedule within a day.'''
 
-from lessons import Lesson
+from lesson import Lesson
 from time_of_day import TimeOfDay
+from json import dump
 
 
 class DailySchedule():
     '''Schedule within a day.'''
-    lessons: list[Lesson]
-    events: List[TimeOfDay]
+    lessons: list[Lesson] = []
+    events: list[TimeOfDay] = []
--- lesson.py
+++ lesson.py
@@ -5,8 +5,8 @@ from time_of_day import TimeOfDay
 
 class Lesson():
     '''Specific lesson.'''
-    start_time: TimeOfDay
-    end_time: TimeOfDay
+    start_time: TimeOfDay = TimeOfDay()
+    end_time: TimeOfDay = TimeOfDay()
 
     def __init__(self, start_time: TimeOfDay, end_time: TimeOfDay):
         self.start_time = start_time
@@ -14,6 +14,3 @@ class Lesson():
 
     def __repr__(self):
         return str(self.start_time) + '-' + str(self.end_time)
-
-lesson = Lesson(TimeOfDay(8, 30), TimeOfDay(9, 10))
-print(lesson)
