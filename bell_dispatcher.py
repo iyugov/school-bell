032d21a51306fb0d001f1c4b4a4fccca0a8ffa05
Ivan Yugov <iyugov@gmail.com>
2022-08-07 16:57:47 +0300

    Add Bell Dispatcher module.

---
+++ bell_dispatcher.py
@@ -0,0 +1,10 @@
+'''Dispatcher for bell queue.'''
+
+from schedule_configuration import ScheduleConfiguration
+
+
+class BellDispatcher():
+    '''Dispatcher for bell queue.'''
+
+    def __init__(self, schedule_configuration: ScheduleConfiguration):
+        pass
