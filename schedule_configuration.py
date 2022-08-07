96439b1155d062964c4c6bba31cbc86dd07cdf44
Ivan Yugov <iyugov@gmail.com>
2022-08-07 11:47:25 +0300

    Update configuration properties.

--- schedule_configuration.py
+++ schedule_configuration.py
@@ -1,6 +1,9 @@
 '''Schedule configuration.'''
 
+from schedule_specific_configuration import ScheduleSpecificConfiguration
+
 
 class ScheduleConfiguration():
     '''Schedule configuration.'''
-
+    global_configuration: ScheduleSpecificConfiguration
+    
--- schedule_specific_configuration.py
+++ schedule_specific_configuration.py
@@ -1,15 +1,22 @@
 '''Schedule specific configuration.'''
 
+from schedule_daily_template import ScheduleDailyTemplate
+
 
 class ScheduleSpecificConfiguration():
     '''Schedule specific configuration.'''
     sound_file_names: dict[str, str]
+    daily_template: ScheduleDailyTemplate
 
     def __init__(self):
         self.sound_file_names = {
                                  'event': '',
                                  'lesson_start': '',
                                  'lesson_end': ''}
+        self.daily_template = ScheduleDailyTemplate('')
 
     def json_repr(self):
-        return self.sound_file_names
+        return {
+                'sounds' : self.sound_file_names,
+                'daily_template': self.daily_template.json_repr()
+               }
