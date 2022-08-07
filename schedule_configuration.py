ea7c43fcc44b31ab8339269c07ba49e25cc81193
Ivan Yugov <iyugov@gmail.com>
2022-08-07 12:56:23 +0300

    Add weekdays and days configurations.

--- schedule_configuration.py
+++ schedule_configuration.py
@@ -1,9 +1,12 @@
 '''Schedule configuration.'''
 
+from datetime import datetime
 from schedule_specific_configuration import ScheduleSpecificConfiguration
 
 
 class ScheduleConfiguration():
     '''Schedule configuration.'''
     global_configuration: ScheduleSpecificConfiguration
+    weekdays_configuration: list[ScheduleSpecificConfiguration]
+    days_configuration: dict[datetime, ScheduleSpecificConfiguration]
     
