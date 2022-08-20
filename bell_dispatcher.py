584ac621d74efdc403768ce558d6cb11558f81d5
Ivan Yugov <iyugov@gmail.com>
2022-08-20 15:58:17 +0300

    Add Test module.

--- bell_dispatcher.py
+++ bell_dispatcher.py
@@ -1,10 +1,11 @@
 '''Dispatcher for bell queue.'''
 
 from schedule_configuration import ScheduleConfiguration
-
+from queue import Queue
 
 class BellDispatcher():
     '''Dispatcher for bell queue.'''
+    bell_queue: Queue
 
     def __init__(self, schedule_configuration: ScheduleConfiguration):
-        pass
+        bell_queue = Queue()
---
+++ test.py
@@ -0,0 +1,3 @@
+'''Test environment.'''
+
+from schedule_configuration_group import ScheduleConfigurationGroup
