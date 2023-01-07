669155ce836884ac92c0f5f6f5dfaf9973789711
Ivan Yugov <iyugov@gmail.com>
2023-01-08 01:28:30 +0300

    Draw sample GUI.

--- gui_test.py
+++ gui_test.py
@@ -3,5 +3,10 @@
 from tkinter import *
 
 root = Tk()
-
+ent = Entry(root, width=20)
+but = Button(root, text="Преобразовать")
+lab = Label(root, width=20, bg='black', fg='white')
+ent.pack()
+but.pack()
+lab.pack()
 root.mainloop()
